987372a25b8ff4f47a534dbf4e3417448c76bf54
Pete Hernandez <pete.g.hernandez@gmail.com>
2020-08-28 17:12:57 -0500

    add the api

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,31 @@
 $(document).ready(function(){
 
+$("#search-button").on("click", function(){
+   event.preventDefault();    
+  var city = $("#search-value").val();
+   weather(city);
+});
 
 
 
-console.log("before the click");
-
-var city ="san antonio";  
-
- var api = "e6cbb66fefd604779e451fd9dd1fdb04"
 
 
-var queryURL = "api.openweathermap.org/data/2.5/weather?q="+ city+"&appid="+ api;
+
+function weather(city) {
+ 
+  var api = "e6cbb66fefd604779e451fd9dd1fdb04";
+  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api;
 
 $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-
+   console.log(response);
  console.log("inside the function");
 
 
   });
+}
 
 
-});
+}); 
