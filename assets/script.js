6c1a7d723255d91f32a25cde4ac6182fc86d57ca
Pete Hernandez <pete.g.hernandez@gmail.com>
2020-08-29 23:15:51 -0500

    added the date and reload with previous city

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,54 @@
 $(document).ready(function(){
 
+
+var cityTracker = 0;
+var lastCities = [];
+let key = "cities";
+
+
+
+//this function will reload the prevous city
+function prefCities(){
+
+  let lastCity= localStorage.getItem(key, lastCities[cityTracker]);
+
+  if(lastCity === null){
+    return;
+  }else{
+    weather(lastCity);
+  }
+}
+
+prefCities();
+
+
+
 $("#search-button").on("click", function(){
     event.preventDefault();    
     var city = $("#search-value").val();
+    
     $("#search-value").val("");
  
     weather(city);
     
+    var lastList = "#list" + cityTracker;
+    $(lastList).text(city);
+
+    //this well set the city to local storage 
+    lastCities[cityTracker]= city;
+    cityTracker++;
+    localStorage.setItem(key, lastCities[cityTracker]);
+    if(cityTracker === 5){
+      console.log("in if statment reset tracker");
+      cityTracker = 0;
+    }
+    
+    var lastList = "#lastList" + cityTracker;
     
 });
 
 //this function will display current info
-function locationInfo(cityLocation, temp, humidity, windSpeed, weatherEl){
+function locationInfo(cityLocation, temp, humidity, windSpeed, weatherEl, date){
   
   $("#location").empty();
   $("#temp").empty();
@@ -19,7 +56,7 @@ function locationInfo(cityLocation, temp, humidity, windSpeed, weatherEl){
   $("#wind").empty();
 
 
-  $("#location").append(cityLocation, weatherEl);
+  $("#location").append(cityLocation, date, weatherEl);
   $("#temp").append(temp);
   $("#humidity").append(humidity);
   $("#wind").append(windSpeed);
@@ -38,7 +75,8 @@ $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-   console.log(response);
+   
+    console.log(response);
    
     var cityLocation = response.name;
     var temp = "Temperature: " + response.main.temp + " °F";
@@ -46,6 +84,8 @@ $.ajax({
     var windSpeed = "Wind Speed: " + response.wind.speed + " MPH";
     var latitude = response.coord.lat;
     var longitude = response.coord.lon;
+    
+    var date =" "+ moment().subtract(10, 'days').calendar() + " ";
     
     //weather icon linking
     var weatherIcon = response.weather[0].icon;
@@ -56,8 +96,13 @@ $.ajax({
      //these functions are now called to display info of the weather
     getforecast(city);
     uvIndex(api, latitude, longitude);
-    locationInfo(cityLocation, temp, humidity, windSpeed, weatherEl);
+    locationInfo(cityLocation, temp, humidity, windSpeed, weatherEl, date);
    
+
+    //local storage
+  let key = "cities";
+
+   localStorage.setItem(key, city);
 
   });
 
@@ -73,25 +118,25 @@ function getforecast(city) {
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-   console.log(response);
-   console.log("inside the Forecast function");
+   
     
    var forecastArray = response.list;
      for(var i = 0, j = 1; i < forecastArray.length; i+= 8, j++){
         
        
-        var forecastTemp = "Temp: " + forecastArray[i].main.temp + " °F";
+       //this will convert the day
         var day = (forecastArray[i].dt_txt.split('-')[2].split(' ')[0]);
         var month = (forecastArray[i].dt_txt.split('-')[1].split(' ')[0]);
         var year = (forecastArray[i].dt_txt.split('-')[0].split(' ')[0]);
         var forecastDate = (month + "/"+ day + "/" + year);
-        var forecastHum = "Humidity: " + forecastArray[i].main.humidity + " %";
         
+        var forecastHum = "Humidity: " + forecastArray[i].main.humidity + " %";
+        var forecastTemp = "Temp: " + forecastArray[i].main.temp + " °F";
 
-        console.log(forecastHum); 
+        
        
-        console.log("the date "+ month + "/"+ day + "/" + year);
-       
+        
+       //this is the icon
         var forecastWeatherIcon = forecastArray[i].weather[0].icon;
         var forecastIconURL = "http://openweathermap.org/img/wn/" + forecastWeatherIcon + ".png";
         var forecastWeatherEl =  $("<image>").html("<img src='" + forecastIconURL + "'>");
@@ -110,7 +155,7 @@ function getforecast(city) {
 
 function displayForecast(forecastDate, forecastTemp, forecastWeatherEl, forecastHum, j){
   
-  console.log(j)
+  
      var forecastD = "#forecastDate" + j;
      var forecastI = "#iconForeCast" + j;
      var forecastT = "#forecastTemp" + j;
